19f365c3301a1bd5ba7ca897afe3b7e0b4592e23
Julien Maupetit <julien@maupetit.net>
2013-04-02 22:20:52 +0200

    Consider using the Compass-sass framework

--- Gruntfile.js
+++ Gruntfile.js
@@ -40,6 +40,9 @@ module.exports = function(grunt) {
 
 		sass: {
 			main: {
+				options: {
+					'compass': true
+				},
 				files: {
 					'css/theme/default.css': 'css/theme/source/default.scss',
 					'css/theme/beige.css': 'css/theme/source/beige.scss',
